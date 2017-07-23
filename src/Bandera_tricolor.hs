cee65cc7b05096f374a7793281a2ccc0b16c212a
Jose A. Alonso <JoseA.Alonso@gmail.com>
2017-07-23 17:22:55 +0200

    Bandera_tricolor con doctest y haddock

--- src/Bandera_tricolor.hs
+++ src/Bandera_tricolor.hs
@@ -1,10 +1,9 @@
--- Bandera_tricolor.hs
--- La bandera tricolor.
--- José A. Alonso Jiménez <jalonso@us.es>
--- Sevilla, 23 de Abril de 2014
--- ---------------------------------------------------------------------
-
--- ---------------------------------------------------------------------
+-- |
+-- Module      : Bandera_tricolor.hs 
+-- Description : La bandera tricolor.
+-- Copyright   : José A. Alonso (23 de Abril de 2014)
+-- License     : GPL-3
+-- 
 -- El problema de la bandera tricolor consiste en lo siguiente: Dada un
 -- lista de objetos xs que pueden ser rojos, amarillos o morados, se pide
 -- devolver una lista ys que contiene los elementos de xs, primero los
@@ -12,22 +11,29 @@
 -- 
 -- Definir el tipo de dato Color para representar los colores con los
 -- constructores R, A y M correspondientes al rojo, azul y morado y la
--- función 
---    banderaTricolor :: [Color] -> [Color]
+-- función
+--
+-- > banderaTricolor :: [Color] -> [Color]
+-- 
 -- tal que (banderaTricolor xs) es la bandera tricolor formada con los
 -- elementos de xs. Por ejemplo,
---    banderaTricolor [M,R,A,A,R,R,A,M,M]  ==  [R,R,R,A,A,A,M,M,M]
---    banderaTricolor [M,R,A,R,R,A]        ==  [R,R,R,A,A,M]
--- ---------------------------------------------------------------------
+--
+-- >>> banderaTricolor [M,R,A,A,R,R,A,M,M]
+-- [R,R,R,A,A,A,M,M,M]
+-- >>> banderaTricolor [M,R,A,R,R,A]
+-- [R,R,R,A,A,M]
 
-module Bandera_tricolor where
+module Bandera_tricolor
+  (banderaTricolor
+  , verifica_banderaTricolor
+  ) where
 
 import Data.List (sort)
+import Test.QuickCheck
 
 data Color = R | A | M
   deriving (Show, Eq, Ord, Enum)
 
--- 1ª definición (con sort):
 banderaTricolor :: [Color] -> [Color]
 banderaTricolor = sort
 
@@ -59,6 +65,34 @@ banderaTricolor5 xs = aux xs (0,0,0)
           aux (R:ys) (as,rs,ms) = aux ys (  as, 1+rs,   ms)
           aux (M:ys) (as,rs,ms) = aux ys (  as,   rs, 1+ms)
 
+
+-- | (prop_banderaTricolor xs) se verifica si todas las definiciones
+-- de banderaTricolor son equivalentes para xs. Por ejemplo,
+--
+-- >>> prop_banderaTricolor [M,R,A,A,R,R,A,M,M]
+-- True
+-- >>> prop_banderaTricolor [M,R,A,R,R,A]
+-- True
+prop_banderaTricolor :: [Color] -> Bool
+prop_banderaTricolor xs =
+  all (== banderaTricolor xs)
+      [f xs | f <- [ banderaTricolor2
+                   , banderaTricolor3
+                   , banderaTricolor4
+                   , banderaTricolor5]]
+
+-- | Incluye el tipo Color en Arbitray
+instance Arbitrary Color where
+  arbitrary = elements [A,R,M]
+
+-- | Comprueba la equivalencia de las definiciones
+--
+-- >>> verifica_banderaTricolor
+-- +++ OK, passed 100 tests.
+verifica_banderaTricolor :: IO ()
+verifica_banderaTricolor = 
+  quickCheck prop_banderaTricolor
+
 -- Comparaciones:
 --    ghci> let bandera n = concat [replicate n c | c <- [M,R,A]]
 --    
@@ -83,3 +117,8 @@ banderaTricolor5 xs = aux xs (0,0,0)
 --    ghci> length (banderaTricolor5 (bandera 1000000))
 --    3000000
 --    (4.45 secs, 622205356 bytes)
+
+-- Comprobación
+--    > stack exec doctest Bandera_tricolor.hs
+--    Examples: 5  Tried: 5  Errors: 0  Failures: 0
+
